87c31051a7057f9452e970ce6213209c3ef39105
+ <+>
2022-08-26 11:20:44 -0600

    FINAL

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_08_25_225228) do
+ActiveRecord::Schema.define(version: 2022_08_26_151300) do
 
   create_table "admins", force: :cascade do |t|
     t.string "name"
@@ -27,7 +27,7 @@ ActiveRecord::Schema.define(version: 2022_08_25_225228) do
     t.boolean "cart"
     t.boolean "favorite"
     t.boolean "ebook"
-    t.string "published_date"
+    t.integer "published_date"
     t.string "price"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
